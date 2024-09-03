28cd0b3e53c1ff47967f03245243008a2f329c30
felipetop <felipejorgetop@gmail.com>
2024-09-03 20:13:44 -0300

    feat: add character selector

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Character } from '../../model/character.model';
+import { TeamService } from '../../service/team.service';
+import { Team } from '../../model/team.model';
 
 @Component({
   selector: 'app-character-card',
@@ -8,4 +10,17 @@ import { Character } from '../../model/character.model';
 })
 export class CharacterCardComponent {
   @Input() character!: Character;
+  selectedRole: keyof Team = 'manager';  // Valor padrão
+
+  constructor(private teamService: TeamService) {}
+
+  onCardClick(): void {
+    const teamMember = {
+      id: this.character.id,
+      name: this.character.name,
+      image: this.character.image
+    };
+
+    this.teamService.addTeamMember(this.selectedRole, teamMember);
+  }
 }
--- src/app/components/character-card/character-card.module.ts
+++ src/app/components/character-card/character-card.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterCardComponent } from './character-card.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CharacterCardComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [CharacterCardComponent]
 })
