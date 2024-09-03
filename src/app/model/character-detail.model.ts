23f0f40806cb29f2e8e5fadea2540b8e7f618260
felipetop <felipejorgetop@gmail.com>
2024-09-03 06:45:40 -0300

    feat: add detail on characters

--- src/app/model/character-detail.model.ts
+++ src/app/model/character-detail.model.ts
@@ -10,12 +10,23 @@ export interface Character {
     status: string;
     species: string;
     gender: string;
+    type: string;
     origin: {
       name: string;
+      dimension: string;
     };
     location: {
       name: string;
+      dimension: string;
     };
     image: string;
+    episode: Array<episode>
+}
+
+export interface episode {
+    id: string;
+    name: string;
+    air_date: string;
+    episode: string;
 }
   
--- src/app/repository/queries/get-character-by-id.graphql.ts
+++ src/app/repository/queries/get-character-by-id.graphql.ts
@@ -5,14 +5,23 @@ export const getCharacterByIdGraphQl = `
       name
       status
       species
+      type
       gender
       origin {
         name
+        dimension
       }
       location {
         name
+        dimension
       }
       image
+      episode {
+        id
+        name
+        air_date
+        episode
+      }
     }
   }
 `;
