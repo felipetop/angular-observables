e5ecf54cce615b2f5bc752a90fcee2bc38df691b
felipetop <felipejorgetop@gmail.com>
2024-09-03 21:25:12 -0300

    feat: add team

--- src/app/service/team.service.ts
+++ src/app/service/team.service.ts
@@ -1,66 +1,46 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject, Observable } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Observable, toArray } from 'rxjs';
 import { Team, TeamMember } from '../model/team.model';
 import { TeamImpl } from '../model/team.model.impl';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
-  // ReplaySubject com buffer de 3 para manter o histórico das três últimas equipes
-  private team$ = new ReplaySubject<Team>(3);
+  private team$ = new BehaviorSubject<Team>(this.loadInitialTeam());
+  private teamHistory$ = new ReplaySubject<Team>(3);
 
-  constructor() {
-    this.team$.next(new TeamImpl());
+  private loadInitialTeam(): Team {
+    const savedTeam = localStorage.getItem('team');
+    return savedTeam ? JSON.parse(savedTeam) : new TeamImpl();
   }
 
   public setTeam(team: Team): void {
     this.team$.next(team);
+    this.teamHistory$.next(team);
+    localStorage.setItem('team', JSON.stringify(team));
   }
 
   public getTeam(): Observable<Team> {
     return this.team$.asObservable();
   }
 
-  public addTeamMember(role: keyof Team, member: TeamMember): void {
-    this.getTeam().subscribe(currentTeam => {
-      const updatedTeam = {
-        ...currentTeam,
-        [role]: member
-      };
-      this.setTeam(updatedTeam);
-    }).unsubscribe();
+  public getTeamHistory(): Observable<Team[]> {
+    return this.teamHistory$.pipe(toArray());  // Converte as emissões em um array
   }
 
-  public mockTeamMembers: TeamMember[] = [
-    {
-      id: 1,
-      name: 'Rick Sanchez',
-      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-    },
-    {
-      id: 2,
-      name: 'Morty Smith',
-      image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-    },
-    {
-      id: 3,
-      name: 'Summer Smith',
-      image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-    }
-  ];
-  
-  public mockTeam: Team = {
-    manager: this.mockTeamMembers[0],
-    coordinator: this.mockTeamMembers[0],
-    scrumMaster: this.mockTeamMembers[1],
-    productOwner: this.mockTeamMembers[2],
-    developerOne: this.mockTeamMembers[1],
-    developerTwo: this.mockTeamMembers[2],
-    developerThree: this.mockTeamMembers[0],
-    qualityAssuranceOne: this.mockTeamMembers[1],
-    qualityAssuranceTwo: this.mockTeamMembers[2],
-    pet: this.mockTeamMembers[0]  // Se desejar adicionar um mascote
-  };
+  public addTeamMember(role: keyof Team, member: TeamMember): void {
+    const currentTeam = this.team$.getValue();
+    const updatedTeam = {
+      ...currentTeam,
+      [role]: member
+    };
+    this.setTeam(updatedTeam);
+  }
+
+  public clearTeam(): void {
+    localStorage.removeItem('team');
+    this.team$.next(new TeamImpl());
+    this.teamHistory$.next(new TeamImpl());  // Limpa o histórico também
+  }
 }
